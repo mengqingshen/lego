4e4b1568b3e4be3d45a42b1c86fd940ccd41bd13
mengqingshen <mengqingshen@baidu.com>
2016-11-22 23:20:42 +0800

    chrome extension manifest 配置完毕

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,50 @@
 var path = require('path')
 var webpack = require('webpack')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name]-[hash].js'
   },
   module: {
     loaders: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loaders: ['babel-loader', 'vue-loader']
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015'],
+          cacheDirectory: true
+        }
       },
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
       },
       {
+        test: /\.pug$/,
+        loaders: ['pug-loader']
+      },
+      {
         test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-        loader: 'file-loader'
+        loader: 'file-loader',
+        query: {
+          name: 'assets/[name].[ext]?[hash]'
+        }
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
         loader: 'file-loader',
         query: {
-          name: '[name].[ext]?[hash]'
+          name: 'assets/[name].[ext]?[hash]'
         }
       }
     ]
@@ -40,7 +53,19 @@ module.exports = {
     historyApiFallback: true,
     noInfo: true
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/templates/index.pug',
+      hash: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'popup.html',
+      template: 'src/templates/popup.pug',
+      hash: true
+    })
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
@@ -52,9 +77,18 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
+    new CopyWebpackPlugin([
+      {
+        from: 'manifest.json'
+      }
+    ])
+    ,
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
+      },
+      mangle: {
+        except: ['super', '$', 'exports', 'require']
       }
     })
   ])
