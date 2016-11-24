4f4faa63243886a350919a63802416e4beb268d3
mengqingshen <mengqingshen@baidu.com>
2016-11-24 17:55:04 +0800

    解决sass的支持问题

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <el-button @click.native="startHacking">Let's do it</el-button>
-  </div>
+<template lang='pug'>
+  #app
+    el-row
+      el-col(:span="24")
+        .grid-content.bg-purple
+
 </template>
 
 <script>
@@ -26,8 +26,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang='sass' rel="stylesheet/scss">
 body {
   font-family: Helvetica, sans-serif;
 }
+
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         loader: 'style-loader!css-loader'
       },
       {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader?sourceMap'
+      },
+      {
         test: /\.pug$/,
         loaders: ['pug-loader']
       },
