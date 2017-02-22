7986ddb9e5f7c4ebac45397500ca844688dac931
sean <eli01linux@aliyun.com>
2017-02-23 03:19:55 +0800

    代码重构

---
+++ src/components/imgSpider/componentHeader.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+    el-row.header
+        el-col(:span="2")
+            i.el-icon-arrow-left
+        el-col(:span="8")
+            .title(:span="2") 图片爬取
+        el-col(:span="2")
+            .close
+</template>
+<style>
+    .header {
+        padding: 5px;
+        background-color: #1f2d3d;
+        color: #fff;
+        font-size: 20px;
+    }
+</style>
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
---
+++ src/views/popup.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+    #app
+        el-menu(mode="vertical")
+            el-menu-item-group(title="爬虫")
+                el-menu-item(index="1")
+                    i.el-icon-picture
+                    | 图片爬取工具
+
+</template>
+
+<script>
+import logo from '../assets/logo.png'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="sass">
+    @import url('../styles/common.scss');
+    body {
+        width: 200px;
+    }
+</style>
