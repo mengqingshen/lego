9b951593f18bd9362813b86287d4971cf09286ac
sean <eli01linux@aliyun.com>
2017-04-07 00:08:22 +0800

    使用 chrome.storage 替代 localStorage

--- src/iframes/downloader/api/crawl.js
+++ src/iframes/downloader/api/crawl.js
@@ -1,5 +1,7 @@
 import * as VARIABLES from '../store/variables'
 import Crawler from '../../../api/downloader'
+import * as storage from '../../../api/chrome-storage'
+
 export {
   getStringModeOfRegExp
 } from '../../../api/utils.js'
@@ -18,19 +20,7 @@ export default {
    * 获取所有收藏的 css 选择器
    */
   getSelectorsCollected () {
-    let selectorCollected
-    const temp = localStorage.getItem(VARIABLES.SELECTORS_COLLECTED)
-    if (!temp) {
-      return null
-    }
-
-    try {
-      selectorCollected = JSON.parse(temp)
-    }
-    catch (err) {
-      console.error(err)
-    }
-    return selectorCollected
+    return storage.getItem(VARIABLES.SELECTORS_COLLECTED)
   },
 
   /**
@@ -39,7 +29,7 @@ export default {
    * @param {Array} selectorCollected 收藏的 css 选择器数据
    */
   saveSelectorsCollected (selectorsCollected) {
-    localStorage.setItem(VARIABLES.SELECTORS_COLLECTED, JSON.stringify(selectorsCollected))
+    storage.setItem(VARIABLES.SELECTORS_COLLECTED, selectorsCollected)
   },
 
   /**
@@ -48,18 +38,7 @@ export default {
    * @returns{Array}
    */
   getSelectorsHistory () {
-    let selectorsHistory
-    const temp = localStorage.getItem(VARIABLES.SELECTORS_HISTORY)
-    if (!temp) {
-      return null
-    }
-    try {
-      selectorsHistory = JSON.parse(temp)
-    }
-    catch (err) {
-      console.error(err)
-    }
-    return selectorsHistory
+    return storage.getItem(VARIABLES.SELECTORS_HISTORY)
   },
 
   /**
@@ -68,6 +47,6 @@ export default {
    * @param{Array} selectorsHistory
    */
   saveSelectorsHistory (selectorsHistory) {
-    localStorage.setItem(VARIABLES.SELECTORS_HISTORY, JSON.stringify(selectorsHistory))
+    storage.setItem(VARIABLES.SELECTORS_HISTORY, selectorsHistory)
   }
 }
